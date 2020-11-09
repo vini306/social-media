9a0f62a4cbf5d2e8e69e2f983b26ba1f97a7f19b
Vinicius dos Santos Mota da Silva <viniciussantos@iMac-de-Vinicius.local>
2020-11-09 18:33:04 -0300

    Resolve resolvers

---
+++ src/resolvers/posts.ts
@@ -0,0 +1,14 @@
+const { Post } from  '@models/Post';
+
+export default {
+  Query: {
+    async getPosts() {
+      try {
+        const posts = await Post.find();
+        return posts;
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
+  },
+};
